d6537a1a49f21736b17629dddfd8d0052ff32611
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-05-15 13:09:25 +0000

    Simplified SSL setup

--- resources/to/gitlab/gitlab.rb
+++
@@ -1,5 +0,0 @@
-external_url 'https://git.breweryda.com'
-nginx['redirect_http_to_https'] = true
-nginx['ssl_certificate'] = "/etc/gitlab/ssl/my_cert.crt"
-nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/my_cert.key"
-nginx['http2_enabled'] = false
